050035986e2e7498a83e6ed2d1ef0157cf7fe4c3
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-19 21:18:24 +0200

    add git router to get one user by id

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -3,6 +3,8 @@ const userController = require('../controllers/userController')
 
 router.post('/', userController.createUser)
 router.get('/', userController.getAllUsers)
+router.get('/:id', userController.getOneUser)
+
 
 
 module.exports=router
