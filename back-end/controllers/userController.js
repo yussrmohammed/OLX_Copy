71f187540a550099ebe136b310099c9e5270bae9
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-19 20:30:58 +0200

    add create user function

---
+++ back-end/controllers/userController.js
@@ -0,0 +1,37 @@
+const userModel = require('../models/userModel')
+const bcrypt= require('bcryptjs')
+
+const userAPIs={
+    createUser: async (req,res)=>{
+        try {
+            const {name,email,password,phone,gender,role} = req.body
+            const salt = await bcrypt.genSalt(10);
+            const hasedPassword= await bcrypt.hash(password,salt)
+            const newUser= await userModel.create({
+                name:name,
+                email:email,
+                phone:phone,
+                gender:gender,
+                role:role,
+            password:hasedPassword    
+
+            })
+            res.json(newUser)
+            
+            
+
+
+            
+        } catch (error) {
+            return res.status(500).json({mes:error.message})
+            
+        }
+
+
+    },
+
+
+}
+
+
+module.exports= userAPIs
