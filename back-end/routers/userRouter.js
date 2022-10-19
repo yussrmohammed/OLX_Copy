4c2a6d2ef7524bebd4281e451470205243e1ce9e
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-19 22:54:18 +0200

    add delete router to delete user

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -5,7 +5,7 @@ router.post('/', userController.createUser)
 router.get('/', userController.getAllUsers)
 router.get('/:id', userController.getOneUser)
 router.put('/:id', userController.updateUser)
-
+router.delete('/:id', userController.deleteUser)
 
 
 module.exports=router
