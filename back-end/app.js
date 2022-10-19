04f6e085b9ba1850730258bfc75bd270088beab4
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-19 20:27:13 +0200

    add user router

--- back-end/app.js
+++ back-end/app.js
@@ -4,8 +4,11 @@ require('dotenv').config()
 const express= require('express')
 const app= express()
 require('./db/dbConnection')
+ const userRouter= require('./routers/userRouter')
 
+ app.use(express.json());
 
+app.use('/user', userRouter)
 
 app.get('/', (req,res)=>{
     res.status(200).send('OLX_Copy site')
