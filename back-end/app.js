f8e8cbbefd01701bf3aa76a4095cb5df753ccb3d
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-22 00:51:20 +0200

    rearrange middlewares

--- back-end/app.js
+++ back-end/app.js
@@ -2,18 +2,28 @@ require('dotenv').config()
 
 
 const express= require('express')
+var cookieParser = require('cookie-parser')
 const app= express()
 require('./db/dbConnection')
  const userRouter= require('./routers/userRouter')
 
  app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET))
+
+app.get('/', (req,res)=>{
+    
+      // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+  res.status(200).send('OLX_Copy site')
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+
+})
+
 
 app.use('/user', userRouter)
 
-app.get('/', (req,res)=>{
-    res.status(200).send('OLX_Copy site')
 
-})
 
 app.listen(process.env.port ,
     ()=>{
