a8d1c5f4f3795b9187a8c70ba390550424a539e1
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-22 00:53:59 +0200

    add get router to log out

--- back-end/routers/userRouter.js
+++ back-end/routers/userRouter.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
+const {authenticateUser}= require('../middleware/authenticaton')
 
 router.post('/', userController.createUser)
-router.get('/', userController.getAllUsers)
-router.get('/:id', userController.getOneUser)
-router.put('/:id', userController.updateUser)
-router.delete('/:id', userController.deleteUser)
+router.post('/login', userController.logIn)
+router.get('/',authenticateUser, userController.getAllUsers)
+router.get('/logout',authenticateUser, userController.logOut)
+router.get('/:id',authenticateUser,userController.getOneUser)
+router.put('/:id',authenticateUser ,userController.updateUser)
+router.delete('/:id', authenticateUser,userController.deleteUser)
 
 
 module.exports=router
