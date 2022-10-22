31a3a466e3c268d3ef667cbb18b20ab1ccb72772
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-22 23:48:26 +0200

    add shared module in core module

--- front-end/OLX_Copy/src/app/core/core.module.ts
+++ front-end/OLX_Copy/src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CoreComponent } from './core.component';
   ],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    SharedModule
   ]
 })
 export class CoreModule { }
