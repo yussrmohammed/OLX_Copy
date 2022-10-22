52e2bbfa7b7986281634f7b8e8ea90a1dc7c2742
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-10-22 20:07:28 +0200

    add category controller and update category model
        This commit contains:
            - add the controller functions to the controller
                - createCategory
                - getAllCategories
                - getSingleCategory
                - updateCategory
                - deleteCategory
            - update the category model

--- back-end/models/categoryModel.js
+++ back-end/models/categoryModel.js
@@ -3,11 +3,20 @@ const mongoose = require('mongoose')
 const categorySchema = mongoose.Schema({
     enName:{
         type:String,
-        required:[true,'Please provide the category name in english']
+        required:[true,'Please provide the category name in English'],
+        unique:true
     },
     arName:{
         type:String,
-        required:[true,'Please provide the category name in arabic']
+        required:[true,'Please provide the category name in Arabic'],
+        validate: {
+            validator: function (input) {
+                // check if all input is in arabic and there can be whitespaces
+                return  /^[\u0600-\u06FF-\s]*$/.test(input); 
+            },
+            message: "Enter a valid name in arabic",
+        },
+        unique:true
     }
 })
 
