6ce98e769d86adcf5168d2d5258574695afe8afd
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-10-22 18:55:45 +0200

    update error errorHandler
        - added a validation check
            - checks for the validation error
            - assign both customErr:message and customErr:statusCode

--- back-end/middleware/errorHandler.js
+++ back-end/middleware/errorHandler.js
@@ -2,12 +2,21 @@ const {StatusCodes} = require('http-status-codes')
 
 const errorHandlingMiddlware = async (err,req,res,next)=>{
     console.log(err,err.message)
+    let CustomError = {
+        msg:err.message|| 'Please Try again Later!',
+        statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
+    }
+
+    if(err.name === 'ValidationError'){
+        CustomError.msg = ` Please provide a valid value to this field : ${Object.keys(err.errors)}`
+        CustomError.statusCode = StatusCodes.BAD_REQUEST
+    }
+
+    
 
 
 
-
-
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:'Internal server error please try again after fixing the error'})
+    return res.status(CustomError.statusCode).json({msg:CustomError.msg})
 }
 
 module.exports = errorHandlingMiddlware
