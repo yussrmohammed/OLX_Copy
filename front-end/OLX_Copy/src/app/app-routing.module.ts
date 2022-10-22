e67e49e2564774cc9d3fb16ea900d170cb550fb6
yussrmohammed <yossrmohammed94@gmail.com>
2022-10-22 22:55:15 +0200

    add lazy loading for admin module

--- front-end/OLX_Copy/src/app/app-routing.module.ts
+++ front-end/OLX_Copy/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }
+const routes: Routes = [{ path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+
+
+
+
+ 
+
+{ path: 'core', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 
 
 
