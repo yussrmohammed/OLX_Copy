79ca3e0f71c98bbcd92e43a086324b10a3234966
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-10-22 20:05:25 +0200

    update errorHandler middleware
        This commit contains:
            - handle the duplication error on the validation done on the
                model
            - assign the custom error message and status code

--- back-end/middleware/errorHandler.js
+++ back-end/middleware/errorHandler.js
@@ -14,7 +14,13 @@ const errorHandlingMiddlware = async (err,req,res,next)=>{
 
     if(err.name === 'CastError'){
         CustomError.msg = `No item found with id : ${err.value}`;
-        CustomError.statusCode = 404;
+        CustomError.statusCode = StatusCodes.NOT_FOUND;
+    }
+    
+    if(err.code && err.code === 11000){
+        console.log(`this is the duplication error ----------> ${Object.keys(err.keyValue)}`)
+        CustomError.msg = `Duplicate Value For : ${Object.keys(err.keyValue)} already exists! Please enter a new value`;
+        CustomError.statusCode = StatusCodes.BAD_REQUEST;    
     }
 
     return res.status(CustomError.statusCode).json({msg:CustomError.msg})
