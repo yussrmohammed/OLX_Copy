08cccc6f00ae5ccca97020cbb516e9d6212bd419
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-10-22 02:59:45 +0200

    start the server using express.js

--- back-end/app.js
+++ back-end/app.js
@@ -3,21 +3,19 @@ require('dotenv').config()
 
 const express= require('express')
 var cookieParser = require('cookie-parser')
-const app= express()
+const app = express()
 require('./db/dbConnection')
- const userRouter= require('./routers/userRouter')
+const userRouter= require('./routers/userRouter')
 
- app.use(express.json());
+app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET))
 
 app.get('/', (req,res)=>{
-    
-      // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
-  res.status(200).send('OLX_Copy site')
-  // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies)
-
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies)
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies)
+    res.status(200).send('OLX_Copy site')
 })
 
 
