8144e4601454b62eb9136aab7630dcbbab95f9e1
yossr mohammed <yossrmohammed94@gmail.com>
2022-10-23 15:20:31 +0200

    create auth service and user service

---
+++ front-end/OLX_Copy/src/app/core/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+  LogIn(userData:object){
+    return this.http.post("http://localhost:3000/user/login", userData,{ headers: new HttpHeaders({ 'Content-Type': 'application/json' })})
+  }
+  register(userData:object){
+    return this.http.post("http://localhost:3000/user/register", userData , { headers: new HttpHeaders({ 'Content-Type': 'application/json' })})
+
+  }
+}
