ecc0e6a9967de86a1b2f109d20d09c7b1eb43620
yossr mohammed <yossrmohammed94@gmail.com>
2022-10-23 15:18:15 +0200

    add some modules

--- front-end/OLX_Copy/src/app/shared/shared.module.ts
+++ front-end/OLX_Copy/src/app/shared/shared.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimengModule } from './primeng/primeng.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     PrimengModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+ 
   ],
   exports:[
-    PrimengModule
+    PrimengModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
   ]
 })
 export class SharedModule { }
