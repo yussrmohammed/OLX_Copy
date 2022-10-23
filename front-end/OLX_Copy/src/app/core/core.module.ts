77e04c284eab19b5ff1436da8843dd7787b42830
yossr mohammed <yossrmohammed94@gmail.com>
2022-10-23 15:23:22 +0200

    import components

--- front-end/OLX_Copy/src/app/core/core.module.ts
+++ front-end/OLX_Copy/src/app/core/core.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
   declarations: [
-    CoreComponent
+    CoreComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
     CoreRoutingModule,
-    SharedModule
+    SharedModule,
+    
+  ],
+  providers:[
+    AuthService
   ]
 })
 export class CoreModule { }
